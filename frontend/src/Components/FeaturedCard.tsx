205fc57448d3f7cb3f23bf20d4b4e73e6bc923d9
Joshua Gmys <joshuagmys17@icloud.com>
2023-09-22 11:11:35 -0400

    Created Featured Carousel

--- frontend/src/Components/FeaturedCard.tsx
+++ frontend/src/Components/FeaturedCard.tsx
@@ -1,20 +1,63 @@
-import React from "react";
-import { Row, Col, Card } from "react-bootstrap";
+import { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import photo1 from "../Assets/slide1.svg";
+import photo2 from "../Assets/slide2.svg";
+import photo3 from "../Assets/slide3.svg";
+import { Container } from "react-bootstrap";
+import "../Styles/App.css";
 
-function Featured(): JSX.Element {
+function FeatureCard(): JSX.Element {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex: number) => {
+    setIndex(selectedIndex);
+  };
+
   return (
-    <Row className="noPaddingOrMargins featured-row mb-5">
-      <Col>
-        <Card className="text-bg-dark featured-card">
-          <Card.Img variant="top" className="card-img" />
-          <Card.ImgOverlay>
-            <Card.Title>Featured</Card.Title>
-            <Card.Text>Description</Card.Text>
-          </Card.ImgOverlay>
-        </Card>
-      </Col>
-    </Row>
+    <Container
+      style={{ maxWidth: "1500px" }}
+      className="mb-5 position-relative"
+    >
+      <h3 className="position-absolute featured-card-title">Featured</h3>
+      <Carousel
+        activeIndex={index}
+        onSelect={handleSelect}
+        controls={false}
+        className="position-relative"
+      >
+        <Carousel.Item>
+          <img
+            className="d-block w-100 position-relative"
+            src={photo1}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 position-relative"
+            src={photo2}
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 position-relative"
+            src={photo3}
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </Container>
   );
 }
 
-export default Featured;
+export default FeatureCard;
