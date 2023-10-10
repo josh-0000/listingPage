a70038efa28e27865620b13fd4e122ecdffbb2df
Joshua Gmys <joshuagmys17@icloud.com>
2023-10-10 14:45:27 -0400

    adding account information to stripe customer

--- database/initdb/data/Users.sql
+++ database/initdb/data/Users.sql
@@ -1,5 +1,5 @@
 -- Insert Users
-INSERT INTO Users (Username, Email, Password)
+INSERT INTO Users (Email, Password)
 VALUES 
-('Josh', 'joshuagmys00@icloud.com', '1Buddydog1$'),
-('Justin', 'justin@example.com','justin');
+('joshuagmys00@icloud.com', '1Buddydog1$'),
+('justin@example.com','justin');
